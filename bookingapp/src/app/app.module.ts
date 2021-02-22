47bafa7488e0ca770ef584876272f9e052696d1e
muhsina-abdulla <musinabinthabdulla@gmail.com>
2021-02-22 05:14:49 -0800

    Creates component 'event-card'

--- bookingapp/src/app/app.module.ts
+++ bookingapp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { EventCardComponent } from './event-card/event-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomePageComponent
+    HomePageComponent,
+    EventCardComponent
   ],
   imports: [
     BrowserModule,
