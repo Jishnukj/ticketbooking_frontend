7a7ae4e9ef112659e74f4b2d0eff94df8869638d
Deadmau5-droid <72690130+Deadmau5-droid@users.noreply.github.com>
2021-02-23 10:07:48 +0530

    Update app-routing.module.ts

--- bookingapp/src/app/app-routing.module.ts
+++ bookingapp/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventCardComponent } from './event-card/event-card.component';
+import { EventComponent } from './event/event.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PaymentComponent } from './payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage', pathMatch: 'full' },
+  { path: 'homepage', component: HomePageComponent },
+  { path: 'event', component: EventComponent },
+  { path: 'event-card', component: EventCardComponent },
+  { path: 'payment', component: PaymentComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
