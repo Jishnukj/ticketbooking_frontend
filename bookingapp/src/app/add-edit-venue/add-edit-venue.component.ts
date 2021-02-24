8ef3cc3df606dfaf21e3c053bef1c033a104d56a
aliena.shaju <aliena.shaju@ALIENASHAJU>
2021-02-24 15:54:23 +0530

    venue pages changed

--- bookingapp/src/app/add-edit-venue/add-edit-venue.component.ts
+++ bookingapp/src/app/add-edit-venue/add-edit-venue.component.ts
@@ -3,6 +3,15 @@ import { FormGroup,FormBuilder, Validators } from '@angular/forms';
 import{ActivatedRoute, Router} from '@angular/router';
 import { VenueService } from '../services/venue.service';
 import {venues} from '../models/venues'
+/**
+ * On initial loading of this component edit venue page will be rendered with a form
+ * which will have venue name, seating capacity  and ticket price textboxes .
+ * 
+ * If the input textboxes are empty,error messages will be shown since this fields are required.
+ * Input fields are validated for touched and dirty properties.
+ * Failing this validation will show error messages.
+
+ */
 
 @Component({
   selector: 'app-add-edit-venue',
@@ -18,7 +27,7 @@ export class AddEditVenueComponent implements OnInit {
   @ Input() Venues !:venues;
   ngOnInit(): void {
     this.addform = this.fb.group({
-      venueId:['',Validators.required],
+      //venueId:['',Validators.required],
       venueName:['',Validators.required],
       seatingCapacity:['',Validators.required],
       ticketPrice:['',Validators.required],
