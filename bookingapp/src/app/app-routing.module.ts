eae652a396f4006565aff755c131b62661962d84
muhsina-abdulla <musinabinthabdulla@gmail.com>
2021-02-25 18:28:10 +0530

    payment component

--- bookingapp/src/app/app-routing.module.ts
+++ bookingapp/src/app/app-routing.module.ts
@@ -7,21 +7,25 @@ import { EventComponent } from './event/event.component';
 import {AdminHomeComponent} from './admin-home/admin-home.component';
 import {EventDetailComponent} from './event-detail/event-detail.component';
 import{ TicketSaleComponent} from './ticket-sale/ticket-sale.component';
+import { PaymentComponent } from './payment/payment.component';
+
 const routes: Routes = [{path : '',component : HomePageComponent},
 {path: 'venues', component: VenuesComponent},
 {path: 'add-edit-venue', component:AddEditVenueComponent },
 {path: 'events', component: AdminHomeComponent},
 {path: 'event-detail/:id', component: EventDetailComponent},
 {path: 'booking', component: TicketSaleComponent},
-{path : 'event/:id', component: EventComponent},];
+{path : 'event/:id', component: EventComponent},
+{path:'payment/:price',component:PaymentComponent},];
 
 
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  
+ 
+
 exports: [RouterModule]
 })
 export class AppRoutingModule { }
 
-export const RoutingComponent = [HomePageComponent,EventComponent];
+export const RoutingComponent = [HomePageComponent,EventComponent,PaymentComponent];
--- bookingapp/src/app/event/event.component.ts
+++ bookingapp/src/app/event/event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 import {ActivatedRoute} from '@angular/router'
 import { EventService } from './../services/event.service';
 import {Event} from '../models/event';
@@ -14,7 +15,8 @@ export class EventComponent implements OnInit {
 
   constructor(
     private route:ActivatedRoute,
-    private service :EventService
+    private service :EventService,
+    private _router: Router,
     ) { }
   public id! : number;
   public Event!: Event[];
@@ -26,6 +28,9 @@ export class EventComponent implements OnInit {
       console.log(data);
 
     });
+  }
+  gotopayment(price:number){
+    this._router.navigate(['payment/'+price]);
   }
 
 
