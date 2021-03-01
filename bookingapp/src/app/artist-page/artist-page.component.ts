f2d7515cbc3e12df419aadada59f41a36f726acf
muhsina-abdulla <musinabinthabdulla@gmail.com>
2021-03-01 20:20:39 +0530

    Adds add-event route to the add event button

--- bookingapp/src/app/artist-page/artist-page.component.ts
+++ bookingapp/src/app/artist-page/artist-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../services/event.service';
 import { Event } from '../models/event';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../services/user.service';
 import{User} from '../models/user'
 @Component({
@@ -15,6 +15,7 @@ export class ArtistPageComponent implements OnInit {
   constructor(
     private readonly service: EventService,
     private readonly _router: ActivatedRoute,
+    private _rout: Router,
     private readonly userService: UserService
   ) {}
   public Artist!:User;
@@ -26,16 +27,18 @@ export class ArtistPageComponent implements OnInit {
       console.log(this.id);
 
       this.userService.GetUser(this.id).subscribe((user) => {
+        console.log(user);
         this.Artist=user;
         this.Events = events.filter((event) => 
-         
-          
-
           event.artist_name === user.user_name
         );
         console.log(this.Events);
 
       });
     });
+  }
+
+  goToAddEvent(){
+    this._rout.navigate(['add-event']);
   }
 }
